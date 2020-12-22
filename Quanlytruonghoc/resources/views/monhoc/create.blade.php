d66bffaee30be8fab2f5719f420f450a6bf50b6f
thaitc <71971777+thaitc@users.noreply.github.com>
2020-12-22 22:46:32 +0700

    qlth_v1.4

---
+++ Quanlytruonghoc/resources/views/monhoc/create.blade.php
@@ -0,0 +1,78 @@
+@extends('templates.master')
+
+@section('title','Thêm mới môn học')
+
+@section('content')
+
+<div class="page-header">
+    <h4>Quản lý môn học</h4>
+</div>
+
+<?php //Hiển thị thông báo thành công
+?>
+@if ( Session::has('success') )
+<div class="alert alert-success alert-dismissible" role="alert">
+    <strong>{{ Session::get('success') }}</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+</div>
+@endif
+
+<?php //Hiển thị thông báo lỗi
+?>
+@if ( Session::has('error') )
+<div class="alert alert-danger alert-dismissible" role="alert">
+    <strong>{{ Session::get('error') }}</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+</div>
+@endif
+@if ($errors->any())
+<div class="alert alert-danger alert-dismissible" role="alert">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+</div>
+@endif
+<?php //Form thêm mới môn học
+?>
+<p><a class="btn btn-primary" href="{{ url('/monhoc') }}">Về danh sách</a></p>
+<div class="col-xs-4 col-xs-offset-4">
+    <center>
+        <h4>Thêm môn học</h4>
+    </center>
+    <form action="{{ url('/monhoc/create') }}" method="post">
+        <input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}" />
+        <div class="form-group">
+            <label for="tenmon">Tên môn</label>
+            <input type="text" class="form-control" id="tenmon" name="tenmon" placeholder="Tên môn" maxlength="255" required />
+        </div>
+        <div class="form-group">
+            <label for="tengiangvien">Chọn giảng viên</label>
+            <select class="form-control" id="tengiangvien" name="tengiangvien" required>
+                <option value="">-- Chọn giảng viên --</option>
+                @foreach($dsgiangvien as $tengiangvien)
+                <option value="{!! $tengiangvien->id !!}">{!! $tengiangvien->tengiangvien !!}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="tinchi">Tín chỉ</label>
+            <input type="text" class="form-control" id="tinchi" name="tinchi" placeholder="Tín chỉ" maxlength="10" required />
+        </div>
+        
+        <center><button type="submit" class="btn btn-primary">Thêm</button></center>
+    </form>
+</div>
+
+@endsection
---
+++ Quanlytruonghoc/resources/views/monhoc/list.blade.php
@@ -0,0 +1,191 @@
+@extends('templates.master')
+@section('title','Quản lý môn học')
+@section('content')
+<?php //Hiển thị thông báo thành công
+?>
+<div class="page-header">
+    <h4>Quản lý môn học</h4>
+</div>
+@if ( Session::has('success') )
+<div class="alert alert-success alert-dismissible" role="alert">
+    <strong>{{ Session::get('success') }}</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+</div>
+@endif
+<?php //Hiển thị thông báo lỗi
+?>
+@if ( Session::has('error') )
+<div class="alert alert-danger alert-dismissible" role="alert">
+    <strong>{{ Session::get('error') }}</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+</div>
+@endif
+<?php //Hiển thị danh sách học sinh
+?>
+<style>
+    #myImg {
+        border-radius: 5px;
+        cursor: pointer;
+        transition: 0.3s;
+    }
+
+    #myImg:hover {
+        opacity: 0.7;
+    }
+
+    /* The Modal (background) */
+    .modal {
+        display: none;
+        position: fixed;
+        z-index: 9999;
+        padding-top: 50px;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgb(0, 0, 0);
+        background-color: rgba(0, 0, 0, 0.9);
+    }
+
+    /* Modal Content (image) */
+    .modal-content {
+        margin: auto;
+        display: block;
+        width: 50%;
+        max-width: 400px;
+    }
+
+    /* Caption of Modal Image */
+    #caption {
+        margin: auto;
+        display: block;
+        width: 80%;
+        max-width: 700px;
+        text-align: center;
+        color: #ccc;
+        padding: 10px 0;
+        height: 50px;
+    }
+
+    /* Add Animation */
+    .modal-content,
+    #caption {
+        -webkit-animation-name: zoom;
+        -webkit-animation-duration: 0.6s;
+        animation-name: zoom;
+        animation-duration: 0.6s;
+    }
+
+    @-webkit-keyframes zoom {
+        from {
+            -webkit-transform: scale(0)
+        }
+
+        to {
+            -webkit-transform: scale(1)
+        }
+    }
+
+    @keyframes zoom {
+        from {
+            transform: scale(0)
+        }
+
+        to {
+            transform: scale(1)
+        }
+    }
+
+    /* The Close Button */
+    .close {
+        position: absolute;
+        top: 15px;
+        right: 35px;
+        color: #f1f1f1;
+        font-size: 40px;
+        font-weight: bold;
+        transition: 0.3s;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: #bbb;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    /* 100% Image Width on Smaller Screens */
+    @media only screen and (max-width: 700px) {
+        .modal-content {
+            width: 100%;
+        }
+    }
+</style>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="table-responsive">
+            <p><a class="btn btn-primary" href="{{ url('/monhoc/create') }}">Thêm mới</a></p>
+            <table id="DataList" class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Tên môn</th>
+                        <th>Tên giảng viên</th>
+                        <th>Tín chỉ</th>
+                        <th>Sửa</th>
+                        <th>Xóa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php //Vòng lập foreach lấy giá vào bảng
+                    ?>
+                    @foreach($listmonhoc as $key => $monhoc)
+                    <tr>
+                        <td style="text-align: center; vertical-align: middle;">{{ $key+1 }}</td>
+                        <td style="vertical-align: middle;"><a href="#">{{ $monhoc->tenmon }}</a></td>
+                        <td style="text-align: center; vertical-align: middle;">
+                            {{ $monhoc->tengiangvien }}
+                        </td>
+                        <td style="vertical-align: middle;">{{ $monhoc->tinchi }}</td>
+                        
+                        
+                        <td style="text-align: center; vertical-align: middle;"><a href="monhoc/{{ $monhoc->id }}/edit">Sửa</a></td>
+                        <td style="text-align: center; vertical-align: middle;"><a href="monhoc/{{ $monhoc->id }}/delete">Xóa</a></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<div id="myModal" class="modal">
+    <span class="close">&times;</span>
+    <img class="modal-content" id="img01">
+    <div id="caption"></div>
+</div>
+<script>
+    function MymodalImage(e) {
+        // Get the modal
+        var modal = document.getElementById('myModal');
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+        modal.style.display = "block";
+        modalImg.src = e.src;
+        captionText.innerHTML = e.alt;
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+</script>
+@endsection
